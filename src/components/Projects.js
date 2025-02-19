b9fcafdff24efffa912d048b24f3d665318ccb32
Boma Fira Suganda <97821996+PumaSgnd@users.noreply.github.com>
2025-02-19 22:04:59 +0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../css/Projects.css";
 
 const projects = [
-  { title: "Web Portfolio", description: "Website berbasis React Js dan Node.js yang mana website berisi informasi pemilik website.", link: "https://github.com/PumaSgnd/my-portfolio" },
+  { title: "Web Portfolio", description: "Website berbasis React Js dan Node.js yang berisi informasi pemilik website.", link: "https://github.com/PumaSgnd/my-portfolio" },
   { title: "OpenMusic API", description: "Backend API dengan fitur manajemen playlist kolaboratif untuk layanan musik online.", link: "https://github.com/PumaSgnd/Openmusic-apiv3" },
-  { title: "CandyCrush Laundy", description: "Backend API dengan fitur manajemen playlist kolaboratif untuk layanan musik online.", link: "https://github.com/PumaSgnd/WebCandyCrush-Laundry" },
+  { title: "CandyCrush Laundry", description: "Backend API dengan fitur manajemen laundry berbasis web.", link: "https://github.com/PumaSgnd/WebCandyCrush-Laundry" },
 ];
 
 const Projects = () => {
@@ -12,11 +12,13 @@ const Projects = () => {
     <section id="projects">
       <h2>Proyek Website</h2>
       <div className="projects-container">
-        {projects.map((project) => (
-          <div className="project-card" key={project.id}>
+        {projects.map((project, index) => (
+          <div className="project-card" key={index}>
             <h3>{project.title}</h3>
             <p>{project.description}</p>
-            <a href={project.link} className="project-link">Lihat Proyek</a>
+            <a href={project.link} className="project-link" target="_blank" rel="noopener noreferrer">
+              Lihat Proyek
+            </a>
           </div>
         ))}
       </div>
