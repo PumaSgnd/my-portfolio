c0ed773f460db373efe82d4e45a50a1e3bf6aec1
Boma Fira Suganda <97821996+PumaSgnd@users.noreply.github.com>
2025-02-19 19:35:23 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,20 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import "../css/navbar.css";
 
 const Navbar = () => {
   return (
-    <nav style={{
-      background: "#333",
-      padding: "10px 20px",
-      color: "white",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
-      position: "fixed",
-      top: "0",
-      left: "0",
-      width: "100%",
-      zIndex: "1000",
-      boxSizing: "border-box"
-    }}>
-      {/* Logo dan Nama */}
-      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-        <img src={logo} alt="Logo PumaSgnd" style={{ height: "50px" }} />
+    <nav className="navbar">
+      {/* Logo */}
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo PumaSgnd" />
       </div>
 
       {/* Menu Navigasi */}
-      <ul style={{ display: "flex", listStyle: "none", gap: "40px", margin: 0, padding: 0 }}>
-        <li><a href="#about" style={{ color: "white", textDecoration: "none" }}>Tentang</a></li>
-        <li><a href="#projects" style={{ color: "white", textDecoration: "none" }}>Proyek</a></li>
-        <li><a href="#contact" style={{ color: "white", textDecoration: "none" }}>Kontak</a></li>
+      <ul className="navbar-menu">
+        <li><a href="#about">Tentang</a></li>
+        <li><a href="#projects">Proyek</a></li>
+        <li><a href="#contact">Kontak</a></li>
       </ul>
     </nav>
   );
